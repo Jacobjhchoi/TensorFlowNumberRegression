051eb6a12b6f864ce3d0505e9526ff3f4055f7bb
Jacobjhchoi <77601745+Jacobjhchoi@users.noreply.github.com>
2021-07-19 01:45:51 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+# Data
+xs = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0], dtype=float)    #took 9 houses with bedrooms 1,2,3....10 respectively
+ys = np.array([],dtype=float)       #prices of 9 houses in 'hundreds of thousands'
+print("Enter 10 numbers that have a pattern\n")
+for i in range(10):
+    number = float(input("Enter number " + str(i + 1) + ": "))
+    ys = np.append(ys, number)
+
+
+# Model
+model = tf.keras.Sequential([keras.layers.Dense(units=4,input_shape=[1]),
+                             keras.layers.Dense(units=1)])
+
+# Build Model
+model.compile(loss=tf.keras.losses.mae,
+              optimizer=tf.keras.optimizers.Adam(lr=0.01),
+              metrics=["mae"])
+
+# Train Model
+model.fit(xs,ys,epochs=300)
+
+print("Type a number you would like to predict from your pattern: ")
+print('Type "end" to quit')
+flag = True
+while flag:
+    user_number = input("Enter a number: ")
+    if (user_number == "end"):
+        print("Program terminated.")
+        flag = False
+    else:
+        input_array = np.array([])
+        input_array = np.insert(input_array, 0, user_number)
+        print(model.predict(input_array))
